dbec741f62693557d67ef5b3fa84ca769d302285
marcprux <mwp1@cornell.edu>
2024-06-26 20:58:14 -0400

    Add supabase-kt API

--- Sources/SkipSupabase/SkipSupabase.swift
+++ Sources/SkipSupabase/SkipSupabase.swift
@@ -2,10 +2,64 @@
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 
+import Foundation
 #if !SKIP
 @_exported import Supabase
 #else
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.gotrue.FlowType
+import io.github.jan.supabase.createSupabaseClient
+
+//import io.github.jan.supabase.auth.Auth
+//import io.github.jan.supabase.auth.auth
+//import io.github.jan.supabase.gotrue.GoTrue
+//import io.github.jan.supabase.gotrue.gotrue
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.storage.Storage
+import io.github.jan.supabase.storage.storage
+
+import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.query.Columns
+
 #endif
 
-public class SkipSupabaseModule {
+#if SKIP
+
+//public typealias SupabaseClient = io.github.jan.supabase.SupabaseClient
+//
+//public func SupabaseClient(supabaseURL: URL, supabaseKey: String) -> SupabaseClient {
+//    return createSupabaseClient(supabaseUrl: supabaseURL.absoluteString, supabaseKey: supabaseKey) {
+//        install(Postgrest)
+//    }
+//}
+
+public class SupabaseClient {
+    private let client: io.github.jan.supabase.SupabaseClient
+
+    public init(supabaseURL: URL, supabaseKey: String) {
+        self.client = createSupabaseClient(supabaseUrl: supabaseURL.absoluteString, supabaseKey: supabaseKey) {
+            // defaultSerializer = JacksonSerializer() // TODO: custom serializer that uses Skip Encodable/Decodable
+
+            //install(Auth)
+            install(Postgrest)
+            install(Storage) {
+                //transferTimeout = 120.seconds // Default: 120 seconds
+            }
+        }
+    }
+
+    public func from(_ tableName: String) -> PostgrestQueryBuilder {
+        PostgrestQueryBuilder(builder: client.from(tableName))
+    }
 }
+
+public class PostgrestQueryBuilder {
+    private let builder: io.github.jan.supabase.postgrest.query.PostgrestQueryBuilder
+
+    internal init(builder: io.github.jan.supabase.postgrest.query.PostgrestQueryBuilder) {
+        self.builder = builder
+    }
+}
+
+#endif
--- Tests/SkipSupabaseTests/SkipSupabaseTests.swift
+++ Tests/SkipSupabaseTests/SkipSupabaseTests.swift
@@ -9,37 +9,46 @@ import Foundation
 
 let logger: Logger = Logger(subsystem: "SkipSupabase", category: "Tests")
 
+struct Country: Codable {
+  let id: Int
+  let name: String
+}
+
 @available(macOS 13, *)
 final class SkipSupabaseTests: XCTestCase {
     func testSkipSupabase() async throws {
         logger.log("running testSkipSupabase")
-        #if !SKIP
         let client = SupabaseClient(
             supabaseURL: URL(string: "https://zncizygaxuzzvxnsfdvp.supabase.co")!,
             supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuY2l6eWdheHV6enZ4bnNmZHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NjE1NDksImV4cCI6MjAyMzQzNzU0OX0.yoFteItT4FVu_kbMuMnQCzE8YYU5jEVWLU7NDBY94-E"
         )
 
-        struct Country: Codable {
-          let id: Int
-          let name: String
-        }
-
         // clear the countries table
-        try await client
-            .from("countries")
+        let cquery = client.from("countries")
+
+        #if !SKIP
+
+        try await cquery
             .delete()
             .gte("id", value: 0)
             .execute()
 
+        // insert single
+        try await client
+            .from("countries")
+            .insert(Country(id: 1, name: "USA"))
+            .execute()
+
+        // insert array
         try await client
             .from("countries")
             .insert([
-                Country(id: 1, name: "USA"),
                 Country(id: 2, name: "France"),
                 Country(id: 3, name: "Germany"),
             ])
             .execute()
 
+        // count query
         let countryCount = try await client
             .from("countries")
             .select(count: .exact)
@@ -47,21 +56,25 @@ final class SkipSupabaseTests: XCTestCase {
 
         XCTAssertEqual(3, countryCount.count)
 
+        // query single row
         let countries: [Country] = try await client
           .from("countries")
           .select()
           .order("id")
+          .limit(1)
           .execute()
           .value
 
         XCTAssertEqual("USA", countries.first?.name)
 
+        // update
         try await client
           .from("countries")
           .update(["name": "Australia"])
           .eq("id", value: 1)
           .execute()
 
+        // verify query
         let countries2: [Country] = try await client
           .from("countries")
           .select()
